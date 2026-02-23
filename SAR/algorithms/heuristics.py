97b7c4e9eb1c7c52f8214c9e9f559de285de1112
LuisHBM111 <l.bobadillam@uniandes.edu.co>
2026-02-22 19:48:50 -0500

    survivorHeuristic (punto5)

--- SAR/algorithms/heuristics.py
+++ SAR/algorithms/heuristics.py
@@ -44,5 +44,22 @@ def survivorHeuristic(state: Tuple[Tuple, Any], problem: MultiSurvivorProblem):
     - Consider: distance to nearest survivor + MST of remaining survivors
     - Balance heuristic strength vs. computation time (do experiments!)
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, survivors_grid = state
+    survivors = survivors_grid.asList()
+
+    if not survivors:
+        return 0
+
+    def manhatan(a, b):
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+    far_from_pos = max(manhatan(position, s) for s in survivors)
+
+    diam = 0
+    for i in range(len(survivors)):
+        for j in range(i + 1, len(survivors)):
+            d = manhatan(survivors[i], survivors[j])
+            if d > diam:
+                diam = d
+
+    return max(far_from_pos, diam)
